19448f519e5a6ff2a2ac6ed5195d530bb78cef01
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-05-12 20:12:47 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+// script.js
+
+function toggleTheme() {
+  var themeIcon = document.getElementById("theme-icon");
+  var body = document.getElementsByTagName("body")[0];
+  
+  if (body.classList.contains("dark-theme")) {
+    body.classList.remove("dark-theme");
+    themeIcon.src = "sun-icon.png";
+  } else {
+    body.classList.add("dark-theme");
+    themeIcon.src = "moon-icon.png";
+  }
+}
 
