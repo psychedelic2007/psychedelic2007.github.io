f08cbd089078f6f26469ed4015ee2d15d1a05878
Satyam Sangeet <51230487+psychedelic2007@users.noreply.github.com>
2023-05-12 15:44:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-<script>
-  function toggleTheme() {
-    document.body.classList.toggle('light');
-    document.querySelector('header').classList.toggle('light');
-    document.querySelector('footer').classList.toggle('light');
-  }
+function toggleTheme() {
+  document.body.classList.toggle('light');
+  document.querySelector('header').classList.toggle('light');
+  document.querySelector('footer').classList.toggle('light');
+}
 
+document.addEventListener('DOMContentLoaded', function() {
   const themeButton = document.querySelector('#themeButton');
   themeButton.addEventListener('click', function() {
-    document.body.classList.contains('light') ? document.body.classList.remove('light') : document.body.classList.add('light');
+    toggleTheme();
   });
-</script>
+});
